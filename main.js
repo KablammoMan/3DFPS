75a8d8ba47fa7146ead2fd03f7a77b24869f0fe3
KablammoMan <codernaffilio@gmail.com>
2023-11-24 08:49:46 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
 const canvas = document.createElement("canvas");
+canvas.width = window.innerWidth * 0.99;
+canvas.height = window.innerHeight * 0.99;
+canvas.classList.add("center");
+document.body.appendChild(canvas);
