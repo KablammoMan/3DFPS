59a71d939775d9bf81368bef7947454e3393cdc1
KablammoMan <codernaffilio@gmail.com>
2023-11-24 14:12:01 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-const canvas = document.createElement("canvas");
-canvas.width = window.innerWidth * 0.99;
-canvas.height = window.innerHeight * 0.99;
-canvas.classList.add("center");
-document.body.appendChild(canvas);
+class Canvas {
+    constructor(xscal=0.99, yscal=0.99, cent=true) {
+        const canvas = document.createElement("canvas");
+        canvas.width = window.innerWidth * xscal;
+        canvas.height = window.innerHeight * yscal;
+        if (cent) canvas.classList.add("center");
+        document.body.appendChild(canvas);
+    }
+}
+
+const gameCanvas = new Canvas()
